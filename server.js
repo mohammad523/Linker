e3a3f443c9c45d7a1b0c2ff66e7fd1c3717fc817
Mohammad <its_mh@outlook.com>
2022-10-14 11:23:55 +0500

    .

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const app = express();
 // connect database
 connectDB();
 
-app.get("/", (req, res) => res.send("API Running"));
-
 // init middleware
 
 app.use(express.json({ extended: false }));
@@ -29,6 +27,16 @@ app.use("/api/events", require("./routes/api/events"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/users", require("./routes/api/users"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 

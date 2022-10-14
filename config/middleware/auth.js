b61586008dcb6436af52a7f524a1b2580edf194a
Mohammad <its_mh@outlook.com>
2022-10-14 14:35:04 +0500

    successfully created middleware.

--- config/middleware/auth.js
+++ config/middleware/auth.js
@@ -3,4 +3,39 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-module.exports = function (req, res, next) {};
+/**
+ * middleware is the middleman between the server and the client.
+ * it process the jwt token from the req.header, then verify, then decrypt
+ */
+
+module.exports = function (req, res, next) {
+	// get token from header
+	const token = req.header("x-auth-token");
+
+	console.log("token", token);
+	console.log("request object", req);
+
+	// check if no token
+	if (!token) {
+		return res.status(401).json({ msg: "No token, authorization denied." });
+	}
+
+	// Verify token
+
+	try {
+		// decrypt token
+		const decoded = jwt.verify(token, config.get("jwtSecret"));
+
+		console.log("decoded", decoded);
+
+		// this will connect the payload
+		req.user = decoded;
+
+		next();
+	} catch (error) {
+		// if token is not valid, this will run
+
+		console.log("res object", res);
+		res.status(401).json({ msg: "Token is not valid" });
+	}
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,12 +2,13 @@
 
 const express = require("express");
 const router = express.Router();
+const auth = require("../../config/middleware/auth");
 
 /**
  * route    GET api/auth
  * desc     Test route
  * access   Public
  */
-router.get("/", (req, res) => res.send("Auth Route"));
+router.get("/", auth, (req, res) => res.send("Auth Route"));
 
 module.exports = router;

68a1db25346ef4b22e69e6ea703388bd23aac8ff
Mohammad <its_mh@outlook.com>
2022-10-14 11:25:25 +0500

    .

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ app.use("/api/users", require("./routes/api/users"));
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
 	// Set static folder
-	app.use(express.static("client/build"));
+	app.use(express.static("Client/build"));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+		res.sendFile(path.resolve(__dirname, "Client", "build", "index.html"));
 	});
 }
 
